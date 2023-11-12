777e1bd1966d33d4b2f1fe6e3f3c3485e09dd072
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-12 11:43:54 -0300

    Added swagger

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
+const swaggerUi = require("swagger-ui-express");
 
 const config = require("../config.js");
 const user = require("./components/user/network");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const swaggerDoc = require("./swagger.json");
 
 // ROUTER
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log("API listening in port ", config.api.port);
